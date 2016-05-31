6b0c620f7d4722c2bfa8e792f9cf5107f90eac2e
frangybeanz <phil.f.hughes@gmail.com>
2016-05-31 21:50:10 +0100

    Added drop pin on long touch
    Drop pin on long map touch
    loading screen

--- Virtual Tourist, Phil!/MapViewController.swift
+++ Virtual Tourist, Phil!/MapViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Phillip Hughes on 25/05/2016.
 //  Copyright © 2016 Phillip Hughes. All rights reserved.
+// Pin drop on long touch reference: http://stackoverflow.com/questions/3959994/how-to-add-a-push-pin-to-a-mkmapviewios-when-touching
 //
 
 import UIKit
@@ -11,17 +12,58 @@ import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet var MapView: MKMapView!
+    var location: CLLocation!
+    var LongPress = UILongPressGestureRecognizer.self
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.mapView.delegate = self
         
-        // Get the stack
-        let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        
+        //Load the long press recogniser when the page loads
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.handleLongPress(_:)))
+            longPressRecogniser.minimumPressDuration = 1.5 //user must hold the press for 1.5 seconds for the pin to drop
+            mapView.addGestureRecognizer(longPressRecogniser)
     }
+    
+    func handleLongPress(getstureRecognizer : UIGestureRecognizer){
+        if getstureRecognizer.state != .Began { return }
+        
+        let touchPoint = getstureRecognizer.locationInView(self.mapView)
+        let touchMapCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        
+        let annotation = MKPointAnnotation()
+        
+        //TODO - Animate the Pin Drop
+
+        // Add the Pin to the map
+        annotation.coordinate = touchMapCoordinate
+        mapView.addAnnotation(annotation)
+        print("I'm in handleLong")
+    }
+   
+    /*
+    func PinOptions(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        print("I see your function, but i choose to ignore it!")
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.animatesDrop = true
+            pinView!.pinTintColor = UIColor.orangeColor()
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    */
 
     //TODO Drop pin
+       //Adding annotations
     
     //TODO Save the Pins location in NS User Defaults
     
